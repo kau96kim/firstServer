0956b624f34c062181b14eefa025791008de21ef
Jong <jhkim@babtalk.com>
2018-10-10 18:02:50 +0900

    views.py: Change to RESTful Class view

--- bbs/urls.py
+++ bbs/urls.py
@@ -3,8 +3,9 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from bbs import views
 
 urlpatterns = [
-    path('', views.bbs_list),
-    path('<int:pk>/', views.bbs_detail),
+    path('', views.BbsList.as_view()),
+    path('<int:pk>/', views.BbsDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
+
--- bbs/views.py
+++ bbs/views.py
@@ -1,3 +1,21 @@
+# 클래스 기반의 Rest CRUD 처리
+from bbs.models import Bbs
+from bbs.serializers import BbsSerializer
+from rest_framework import generics
+
+
+# generics 에 목록과 생성 API 가 정의되어 있다
+class BbsList(generics.ListCreateAPIView):
+    queryset = Bbs.objects.all()
+    serializer_class = BbsSerializer
+
+
+# generics 에 상세, 수정, 삭제 API 가 정의되어 있다
+class BbsDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Bbs.objects.all()
+    serializer_class = BbsSerializer
+
+"""
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -43,4 +61,4 @@ def bbs_detail(request, pk, format=None):
     elif request.method == 'DELETE':
         bbs.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
-
+"""
