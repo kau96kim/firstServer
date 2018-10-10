6729ab8876392c594c66c4c75d5c58b01efa2177
Jong <jhkim@babtalk.com>
2018-10-10 15:09:53 +0900

    First commit. Initialize memo app / add model / Set timezone / add superuser

---
+++ memo/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from memo.models import Memo
+
+
+class MemoAdmin(admin.ModelAdmin):
+    list_display = ('content', 'last_modified')
+
+
+admin.site.register(Memo, MemoAdmin)
+
